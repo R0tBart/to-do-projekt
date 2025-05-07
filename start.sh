3a75511a062ac62cc63d777fee096505091a45dc
Chris | R0tBart | techstarter <christoph.von.hagen@tn.techstarter.de>
2025-05-07 15:24:53 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "🔁 Pulling latest Docker images..."
+docker pull christechstarter/todo-frontend:latest
+docker pull christechstarter/todo-backend:latest
+
+echo "🚀 Starting application with docker-compose..."
+docker-compose --env-file .env up -d
+
+echo "✅ Done. Access the app at: http://localhost:3000"
